57d95b4c859d581f333250378b6a9759e7970efe
Lance Bragstad <lbragstad@gmail.com>
2015-07-21 18:22:36 +0000

    fixup testing to work properly

--- test_federation_exercises.py
+++ test_federation_exercises.py
@@ -1,21 +1,19 @@
-import json
 import os
+import requests
 import unittest
 
+from keystoneauth1.auth.identity.v3 import k2k as k2k_plugin
+from keystoneauth1.auth.identity.v3 import password as password_plugin
 from keystoneclient.auth.identity import v3
 from keystoneclient import session
 from keystoneclient.v3 import client
 
 # Service Provider information
 SP_IP = os.environ.get('OS_SP_IP')
-SP_CERT = os.environ.get('OS_SP_CERT')
-SP_KEY = os.environ.get('OS_SP_KEY')
 SP_ENDPOINT = 'https://%s/v3' % SP_IP
 
 # Identity Provider information
 IDP_IP = os.environ.get('OS_IDP_IP')
-IDP_CERT = os.environ.get('OS_IDP_CERT')
-IDP_KEY = os.environ.get('OS_IDP_KEY')
 IDP_ENDPOINT = 'https://%s/v3' % IDP_IP
 
 
@@ -40,7 +38,7 @@ class K2KFederationTestCase(unittest.TestCase):
                         user_domain_name='Default',
                         project_name='admin',
                         project_domain_name='Default')
-        s = session.Session(auth=a, verify=SP_CERT, cert=(SP_CERT, SP_KEY))
+        s = session.Session(auth=a, verify=False)
         c = client.Client(session=s)
 
         domain_name = 'domain1'
@@ -117,7 +115,7 @@ class K2KFederationTestCase(unittest.TestCase):
                         user_domain_name='Default',
                         project_name='admin',
                         project_domain_name='Default')
-        s = session.Session(auth=a, verify=IDP_CERT, cert=(IDP_CERT, IDP_KEY))
+        s = session.Session(auth=a, verify=False)
         c = client.Client(session=s)
 
         sp_id = 'keystone.sp'
@@ -134,110 +132,30 @@ class K2KFederationTestCase(unittest.TestCase):
         if not service_providers:
             c.federation.service_providers.create(**sp_ref)
 
-    def _v3_authenticate(self):
-        self.session = session.Session(
-            auth=v3.Password(auth_url=self.auth_url,
-                             username=self.username,
-                             password=self.password,
-                             user_domain_id=self.domain_id,
-                             project_id=self.project_id),
-            verify=False)
-        self.session.auth.get_auth_ref(self.session)
-        return self.session.auth.get_token(self.session)
+    def test_full_workflow(self):
+        CREDENTIALS = {
+            'auth_url': 'https://104.239.165.190/v3',
+            'username': 'admin',
+            'user_domain_name': 'default',
+            'password': 'password',
+            # project_id of the identity provider
+            'project_id': '92a0b22275284c83b3efc37433751038'
 
-    def _check_response(self, response):
-        if not response.ok:
-            raise Exception("Something went wrong, %s" % response.__dict__)
-
-    def _get_saml2_ecp_assertion(self, token_id):
-        request_body = json.dumps({
-            "auth": {
-                "identity": {
-                    "methods": [
-                        "token"
-                    ],
-                    "token": {
-                        "id": token_id
-                    }
-                },
-                "scope": {
-                    "service_provider": {
-                        "id": self.sp_id
-                    }
-                }
-            }
-        })
-        url = self.auth_url + '/auth/OS-FEDERATION/saml2/ecp'
-        r = self.session.post(url=url, data=request_body, verify=False)
-        self._check_response(r)
-        return str(r.text)
-
-    def _get_service_provider(self):
-        url = self.auth_url + '/OS-FEDERATION/service_providers/' + self.sp_id
-        r = self.session.get(url=url, verify=False)
-        self._check_response(r)
-        sp = json.loads(r.text)[u'service_provider']
-        return sp
-
-    def _handle_http_302_ecp_redirect(self, response, location, **kwargs):
-        return self.session.get(location, authenticated=False, **kwargs)
-
-    def _exchange_assertion(self, assertion):
-        """Send assertion to a Keystone SP and get token."""
-        sp = self._get_service_provider()
-
-        r = self.session.post(
-            sp[u'sp_url'],
-            headers={'Content-Type': 'application/vnd.paos+xml'},
-            data=assertion,
-            authenticated=False,
-            redirect=False)
-
-        self._check_response(r)
-
-        r = self._handle_http_302_ecp_redirect(r, sp[u'auth_url'],
-                                               headers={'Content-Type':
-                                               'application/vnd.paos+xml'})
-        return r
-
-    def test_workflow(self):
-        token_id = self._v3_authenticate()
-        assertion = self._get_saml2_ecp_assertion(token_id)
-        fed_token_response = self._exchange_assertion(assertion)
-        fed_token_id = fed_token_response.headers.get('X-Subject-Token')
-
-        r = self.session.get(
-            url='https://%s/v3/OS-FEDERATION/projects' % SP_IP,
-            headers={'X-Auth-Token': fed_token_id},
-            verify=False)
-        self._check_response(r)
-        projects = json.loads(str(r.text))
-
-        token_data = {
-            "auth": {
-                "identity": {
-                    "methods": [
-                        "token"
-                    ],
-                    "token": {
-                        "id": fed_token_id
-                    }
-                },
-                "scope": {
-                    "project": {
-                        "id": project_id
-                    }
-                }
-            }
         }
 
-        # project_id can be select from the list in the previous step
-        token = json.dumps(token_data)
-        url = 'https://%s/v3/auth/tokens' % SP_IP
-        headers = {'X-Auth-Token': fed_token_id,
-                   'Content-Type': 'application/json'}
-        r = self.session.post(url=url, headers=headers, data=token,
-                              verify=False)
-        self._check_response(r)
-        self.scoped_token_id = r.headers['X-Subject-Token']
-        self.scoped_token = str(r.text)
+        s = session.Session(verify=False)
+        passwd = password_plugin.Password(**CREDENTIALS)
+        K2K_CREDENTIALS = {
+            'base_plugin': passwd,
+            'service_provider': 'keystone.sp',
+        }
+
+        k2k = k2k_plugin.Keystone2Keystone(**K2K_CREDENTIALS)
+        access = k2k.get_access(s)
+
+        fed_token = access.auth_token
+
+        requests.get(
+            url='https://104.239.165.207/v3/auth/projects',
+            headers={'X-Auth-Token': fed_token},
+            verify=False)
